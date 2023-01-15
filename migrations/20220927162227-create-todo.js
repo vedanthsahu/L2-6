0f4e3a118b04b2ae2bcac253cb9e43d18af03382
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-15 21:54:45 +0530

    Updated

--- migrations/20220927162227-create-todo.js
+++ migrations/20220927162227-create-todo.js
@@ -5,7 +5,13 @@ module.exports = {
     await queryInterface.createTable("Todos", {
       id: {
         allowNull: false,
-      
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
       dueDate: {
         type: Sequelize.DATEONLY,
       },
