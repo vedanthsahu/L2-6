a674fff78dce6ee5f9a23554c9a58926e256ab0b
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-15 18:44:19 +0530

    Updated

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
 const express = require("express");
 const app = express();
-var csrf = require("tiny-csrf");
-const bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-app.use(bodyParser.json());
-const path = require("path");
-const { Todo } = require("./models");
-// eslint-disable-next-line no-unused-vars
-const todo = require("./models/todo");
-// eslint-disable-next-line no-undef
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("shh! some secret string"));
-app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
-
-app.set("view engine", "ejs");
-app.get("/", async (request, response) => {
-  const allTodos = await Todo.getTodos();
-  const overdue = await Todo.overdue();
-  const dueLater = await Todo.dueLater();
-  const dueToday = await Todo.dueToday();
-  const completedItems = await Todo.completedItems();
-  if (request.accepts("html")) {
-    response.render("index", {
-      title: "Todo Application",
-      allTodos,
-      overdue,
-      dueLater,
-      dueToday,
-      completedItems,
-      csrfToken: request.csrfToken(),
-    });
+var csrf = require("tiny-csrf");    });
   } else {
     response.json(overdue, dueLater, dueToday, completedItems);
   }
